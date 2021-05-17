5f304f7c87c301dcbcfebd2110608243f61ee63b
Caleb Tony <calebmbugua@gmail.com>
2021-05-17 15:13:19 +0300

    added location category and photo models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name: models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+class Category(models.Model):
+    title: models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+class Photo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,)
+    category = models.ManyToManyField(Category)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
