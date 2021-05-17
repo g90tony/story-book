ef4445111a14d4a0b11d6112fe9021b73afd544e
Caleb Tony <calebmbugua@gmail.com>
2021-05-17 14:44:50 +0300

    minor fixes

--- app/settings.py
+++ app/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 from decouple import config, Csv
+
 MODE=config("MODE", default='dev')
 SECRET_KEY = config('SECRET_KEY')
 
