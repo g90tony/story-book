a37c9b6905da19813c685a15de3e604fed2f79b7
Caleb Tony <calebmbugua@gmail.com>
2021-05-17 14:08:32 +0300

    removed mimetype

--- app/settings.py
+++ app/settings.py
@@ -128,6 +128,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
