40d535cd74a2b3645ef123343c1902af01dcf30f
Caleb Tony <calebmbugua@gmail.com>
2021-05-18 08:03:11 +0300

    deleted login and register views
    added index view

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def login(request):
-    return render(request, 'login.html')
-
-def register(request):
-    return render(request, 'register.html')
+def index(request):
+    return render(request, 'home.html')
