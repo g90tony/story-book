b310318387ad668612b1f16dbac982ce409a8c2f
Caleb Tony <calebmbugua@gmail.com>
2021-05-18 16:24:37 +0300

    removed sharable link from models into it's own file

--- posts/models.py
+++ posts/models.py
@@ -1,51 +1,31 @@
 from django.db import models
-from cloudinary import CloudinaryImage
-import random 
-import string 
-from decouple import config
+from cloudinary.models import CloudinaryField
+from .link_generator import generate_shareable_link
 
 # Create your models here.
 class Location(models.Model):
-    name: models.CharField(max_length=255)
+    name = models.CharField(max_length=255, default=None)
 
     def __str__(self):
         return self.name
     
 class Category(models.Model):
-    title: models.CharField(max_length=255)
+    title = models.CharField(max_length=255, default=None)
 
     def __str__(self):
         return self.title
 
 class Photo(models.Model):
     
-    def generate_shareable_link(self):
-        letters = string.ascii_letters
-        numbers = string.digits
-        specialCharacters = string.punctuation
-
-        acceptable_characters = letters + numbers + specialCharacters
-        generated_link = "".join(random.choice(acceptable_characters) for i in range(8))
-
-        existing_link = Photo.objects.filter(sharable_link = generated_link)
-        
-        if existing_link:
-            self.generate_shareable_link()
-        else:
-            if config("MODE") == 'dev':
-                return f'http://127.0.0.1/photo/{generated_link}'
-            else:
-                return f'http://https://project-storybook.herokuapp.com/photo/{generated_link}'
-    
     sharable_link = generate_shareable_link()
-    
-    title = models.CharField(max_length=255)
+   
+    title = models.CharField(max_length=255, default=None)
     description = models.TextField()
     location = models.ForeignKey(Location, on_delete=models.CASCADE,)
     category = models.ManyToManyField(Category)
     date = models.DateTimeField(auto_now=True)
-    img_url = CloudinaryImage(max_length=255, blank=True)
-    sharable_link = models.CharField(default=sharable_link)
+    img_url = CloudinaryField('image')
+    sharable_link = models.CharField(max_length=255, unique=True, default=sharable_link)
 
     def __str__(self):
         return self.title
