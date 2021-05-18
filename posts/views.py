ca8e0ccf11ddb987080b67d8e04ff7a275d8739e
Caleb Tony <calebmbugua@gmail.com>
2021-05-18 08:25:41 +0300

    added search view function

--- posts/views.py
+++ posts/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home.html')
+
+def search_title(request):
+    return render(request, 'search.html')
