ac9fd9c96413e00c170c4d459cbd78b368d43237
Caleb Tony <calebmbugua@gmail.com>
2021-05-18 10:44:45 +0300

    added get all photos query and title in index
    added filer query by search term and title in seach_title

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from .models import Category, Location, Photo 
 # Create your views here.
 def index(request):
-    return render(request, 'home.html')
+    title = "Home: Welcome to Story Book"
+    allPosts = Photo.objects.all()
+    
+    return render(request, 'home.html', {"title":title ,"posts": allPosts})
 
-def search_title(request):
-    return render(request, 'search.html')
+def search_title(request, search_query):
+    
+    title = f'Search: {search_query} Results'
+    
+    Photo.objects.filter(category = search_query)
+    
+    return render(request, 'search.html', {"title": title})
